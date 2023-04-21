eab7dfa0f70e3c58778694fd332ca03e09f03fc0
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-21 16:57:25 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function is calculates the area of a trapezoid
+*/
+'use strict'
+
+
+function calculate () {
+// input
+const farheneit = parseFloat(document.getElementById('fahrenheit').value)
+
+// process
+
+const celsius = (farheneit - 32) * 5 / 9
+
+// output
+document.getElementById('celsius').innerHTML = 'celsius is: ' + celsius + ' °C'
+}
